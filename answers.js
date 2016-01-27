13bcb8601a7ce472c46f4e6120c19e6463bf17b4
Sonia <sonia.badeau@gmail.com>
2016-01-27 18:20:23 +0000

    answers.js

--- answers.js
+++ answers.js
@@ -1,1 +1,8 @@
 //this is my .js file where I will put my answers
+//Write a function that takes a string and returns the first character of the string. Test your function on a few inputs, including the empty string.
+//This function allows to print out the first letter of the javascript word
+var firstCharacter = function(word){
+    return word.substring(0,1)
+}
+firstCharacter("javascript");
+console.log(firstCharacter("javascript"));
