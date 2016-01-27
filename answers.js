b9cb1c7dae82f270d16d382acdd18f2e09fb7a36
Sonia <sonia.badeau@gmail.com>
2016-01-27 18:28:12 +0000

    answers.js

--- answers.js
+++ answers.js
@@ -5,4 +5,13 @@ var firstCharacter = function(word){
     return word.substring(0,1)
 }
 firstCharacter("javascript");
-console.log(firstCharacter("javascript"));
+//console.log(firstCharacter("javascript")); to test the function with node.js
+
+//Write a function that takes a string and returns the last character of a string. Test your function on a few inputs, including the empty string.
+//This function allows to print out the last character of the javascript word
+var lastCharacter = function(word){
+    var chain = word.length;
+    return word.substring(chain-1,chain);
+};
+lastCharacter("javascript");
+//console.log(lastCharacter("javascript"));
