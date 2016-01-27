926f7c3407493b038bd3f3979e159a9e1e23f3e6
Sonia <sonia.badeau@gmail.com>
2016-01-27 19:11:21 +0000

    answers.js

--- answers.js
+++ answers.js
@@ -3,7 +3,7 @@
 //This function allows to print out the first letter of the javascript word
 var firstCharacter = function(word){
     return word.substring(0,1)
-}
+};
 firstCharacter("javascript");
 //console.log(firstCharacter("javascript")); to test the function with node.js
 
@@ -15,3 +15,13 @@ var lastCharacter = function(word){
 };
 lastCharacter("javascript");
 //console.log(lastCharacter("javascript"));
+
+//Write a function that takes a string and a number, and returns the character at the position represented by the number. 
+//The indexing of number should start at 0. Test your function on a few inputs, including the empty string.
+//This function allows to return the sixth character of the word development
+var stringNumber = function(string, number) {
+    var string = "development";
+    var number = 5;
+    var res = string.charAt(5); 
+};
+
